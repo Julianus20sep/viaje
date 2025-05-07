5f487805449af86228e6aa1ecbf614594552f00c
Julian Montes <aguilor22A@gmail.com>
2025-05-07 18:33:10 -0500

    python borrador

---
+++ curso-py/control-flujo/02-if.py
@@ -0,0 +1,12 @@
+edad = 6
+
+if edad > 65:
+    print("puedes ver la peli con super descuento")
+elif edad > 55:
+    print("pudes ver la peli con descuento")
+elif edad > 17:
+    print("puedes ver la peli")
+else:
+    print("No puedes entrar")
+    print("ve a otro lado pnjo")
+# print("listo,adelante")
